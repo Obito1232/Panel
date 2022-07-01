b86ab9fd9cc3d27ade8a5f877563586f818aad22
Obito1232 <obito1262@gmail.com>
2022-07-01 02:00:45 -0500

    takst end

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\Admin\DashboardController as Admin;
+use App\Http\Controllers\Admin\StatsController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +18,33 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', [HomeController::class, 'index']);
+Route::get('user/{name}', [UserController::class, 'show']);
+
+Route::get('/about', function () {
+    return view('pages.about');
 });
+
+Route::middleware(['auth'])->group(function () {
+   
+    Route::group(['prefix' => 'app'], function()  
+    {  
+        Route::get('dashboard', [DashboardController::class, 'index'])->name('dashboard');
+        Route::get('task', [DashboardController::class, 'task'])->name('task');
+    });  
+});  
+
+Route::middleware(['is_admin'])->group(function () {
+   
+    Route::group(['prefix' => 'admin'], function()  
+    {  
+        Route::get('dashboard', [Admin::class, 'index']);
+        Route::get('stats', [StatsController::class, 'index']);
+
+    });  
+ 
+
+});  
+
+
+
